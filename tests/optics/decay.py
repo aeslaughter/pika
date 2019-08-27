3ed5bbd4208d2e7e4f1fc3134d4b6e85f19d65c6
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-08-27 08:02:01 -0600

    WIP

--- tests/optics/decay.py
+++ tests/optics/decay.py
@@ -5,7 +5,10 @@ import numpy as np
 import mooseutils
 
 data = mooseutils.PostprocessorReader('decay_out.csv')
+
+print data
+
 #fig = mooseutils.ConvergencePlot(np.sqrt(data['dofs']), data['error'], xlabel='sqrt(ndofs)', ylabel='L2 Error')
-fig = mooseutils.ConvergencePlot(data['h'], data['error'], xlabel='Element Length', ylabel='L2 Error')
+#fig = mooseutils.ConvergencePlot(data['h'], data['error'], xlabel='Element Length', ylabel='L2 Error')
 #fig.save('convergence.png')
 fig.show()
